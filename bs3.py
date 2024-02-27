572bd6b6146266cb2d33426ca200243181f5fa12
dogear <akanet.jp@gmail.com>
2024-02-27 13:24:57 +0900

    bs3.py追加

---
+++ bs3.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+res=requests.get('https://joytas.net/kaba/')
+res.encoding=res.apparent_encoding
+
+soup=BeautifulSoup(res.text,'html.parser')
+
+
+#print(soup)
+
+#タグで要素取得
+ele=soup.find('title')
+
+print(ele.text)
+
+imgs=soup.find_all('img')
+for img in imgs:
+    #属性にアクセスすりにはgetメソッドを使う
+    print(img.get('src'))
